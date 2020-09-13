4477556c6c2475e8bde72206323dfd4f00c25ce3
Caique <caiquefssouza.98@gmail.com>
2020-09-13 01:41:05 -0300

    mudança na pagina de produto

--- src/components/main/Prod_page.js
+++ src/components/main/Prod_page.js
@@ -7,60 +7,82 @@ import {
   Text,
   Button,
   Input,
-  ThemeProvider
+  ThemeProvider,
+  Card
 } from 'react-native-elements';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 class Produto extends Component {
+  
   constructor(props){
     super(props);
+    
+    this.state = {desc: '',
+                  prod:{}}
   }
 
-  componentDidMount(){
+  async getProduct(){
     url="http://192.168.15.72:3001/cardapio/"+this.props.prodId;
-    axios.get(url)
-      .then(responseJson => {this.setState({isSearching:false,
-                                            dataSource: responseJson},
-                            function(){this.arrayholder = responseJson.data})})
-      .catch(err => {console.log(err)});
+    await axios.get(url)
+    .then(responseJson => {this.setState({prod: responseJson.data})})
+    .catch(err => {console.log(err)});
+  }
+  
+  componentDidMount(){
+    this.getProduct();
   }
 
   render(){
     return(
-      <View>
-        <Text>X-Bacon</Text>
-        <Text>
-          Descrição: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis ante lectus. Fusce eget sodales lorem.
-          Aenean ut sem turpis. Mauris velit diam, vehicula in orci a, convallis egestas orci.
-        </Text>
-        <Text>R$13.90</Text>
-        <Input 
-          label="Observações"
-        />
+      <ThemeProvider theme={theme}>
         <View>
-          <Button
-            icon={
-              <Icon
-                name="plus"
-                size={15}
-                color="#ff0000"
+          <Card>
+            <Text h2Style style={{color:'#000000', fontSize:18}}>{this.state.prod.nome}</Text>
+            <Text h4Style style={{color:'#7a7a7a', fontSize:13, marginTop:5, marginHorizontal:7}}>{this.state.prod.descricao}</Text>
+            <Text h3Style style={{color:'#ff0000', fontSize:15, marginTop:7} }>{this.state.prod.valor}</Text>
+            <Input 
+              label="Observações"
+            />
+            <View style={{ flexDirection:"row" }}>
+              <Button
+                containerStyle={{position:'relative'}}
+                icon={
+                  <Icon
+                    name="plus"
+                    size={15}
+                    color="#ffffff"
+                  />
+                }
               />
-            }
-          />
-          <Button
-            icon={
-              <Icon
-                name="minus"
-                size={15}
-                color="#ff0000"
+              <Input 
               />
-            }
-          />
+              <Button
+                containerStyle={{position:'relative'}}
+                icon={
+                  <Icon
+                    name="minus"
+                    size={15}
+                    color="#ffffff"
+                  />
+                }
+              />
+            </View>
+          </Card>
         </View>
-      </View>
+      </ThemeProvider>
     )}
 }
+
+const theme ={
+  Button:{
+    buttonStyle: {
+      marginTop:5,
+      padding:10,
+      backgroundColor: 'red',
+      },
+  }
+};
 
 export default Produto;
